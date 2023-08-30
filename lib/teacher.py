29319c7ee392f5ff66d50ac19220929e6e9d8d05
anissalopez <anissa965@gmail.com>
2023-08-30 12:11:53 -0400

    learning inheritance

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,23 @@ import random
 
 class Teacher(User):
 
+
+    def __init__(self, first_name, last_name, knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+    ]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+
+
+my_teacher = Teacher("My", "Teacher")
+print(my_teacher.knowledge)
